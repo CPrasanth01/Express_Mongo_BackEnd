f3003d77466c0bc7c8989077943419a25f2fd536
Prasanth <35705202+CPrasanth01@users.noreply.github.com>
2020-05-27 22:21:25 +0530

    LookUp Aggregate Example

--- routes/todo.js
+++ routes/todo.js
@@ -36,6 +36,26 @@ router.post('/todo',async (req,res,next) => {
     }
 });
 
+//INSERT REF
+router.post('/todo/ref',async (req,res,next) => {
+
+    console.log(req.body);
+    var descInput = {};
+    descInput.name  = req.body.name;
+    descInput.Description = req.body.Description;
+    console.log(descInput);
+    try{
+        const desc  = await getTodoCollection('todo_ext').insertOne(descInput);
+        req.body.Description = desc.insertedId;
+        const result  = await getTodoCollection('todo').insertOne(req.body);
+        res.status(200).json({message:'Data inserted' , insertedId : result.insertedId});
+    } catch (e){
+        console.log(e);
+        res.status(500).json({message: "Error while Inserting"})
+    }
+});
+
+
 //UPDATE ONE
 router.patch('/todo/:name',async (req,res,next) => {
     console.log(req.query.name);
@@ -92,6 +112,58 @@ router.get('/count/:text',async (req,res,next) =>{
     } catch(e){
         console.log(e);
         res.status(500).json({message: "Error Occurred"});
+    }
+});
+
+//lookup example
+router.get('/todo/ref', async (req, res, next)=>{
+    console.log("Routing work");
+    try {
+        const todos = await getTodoCollection('todo').aggregate([{
+             $match: {
+                name :  "Test5"
+                // title: { $regex: /example/ }
+            } 
+        },{
+            $lookup:{
+                from :"todo_ext",
+                localField : "name",//Description
+                foreignField :"name",//_id
+                as :"desc"
+            }
+        }]).toArray();
+        res.status(200).json(todos);
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({message: "Error while Retriving"})
+    }
+});
+
+//Aggregate Framework returns cursor
+// Each pipeline returns data which can only be used in next pipeline
+//1. Match
+//2. Sort
+//3. Group
+//4. project
+router.get('/todo/agg',async (req,res,next) => {
+    console.log(req.params.name);
+    try {
+        console.log("Trying aggregate");
+        const resultAgg = await getTodoCollection('todo').aggregate([{
+            $match: {
+                name :  "Test5"
+                // title: { $regex: /example/ }
+            } 
+        },     // match is like find with criteria
+           { $group : { _id : {Comment : "$Comment"},total : {$sum :1 } } },  //group based on comment and add it in sum 
+           { $sort : {total :1}},                                             // sort is allowed only based on previous pipeline's data
+           { $project : { _id :1 , total :1}}
+        ]).toArray();
+        console.log("Result");
+        res.status(200).json(resultAgg)
+    } catch (e){
+        console.log(e);
+        res.status(500).json({message: "Error Occurred"})
     }
 });
 
